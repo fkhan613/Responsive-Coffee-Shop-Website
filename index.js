620959c41aabb24849f76502dbb8d8a2449ef87f
Farhan Khan <farhan.k2005@gmail.com>
2022-06-03 15:43:51 -0400

    made all buttons click responsive

---
+++ index.js
@@ -0,0 +1,27 @@
+let navbar = document.querySelector(".navbar");
+let cartList = document.querySelector(".cart-items-container");
+let searchBar = document.querySelector(".search-form");
+
+document.querySelector("#menu-btn").onclick = () => {
+  navbar.classList.toggle("active");
+  cartList.classList.remove("active");
+  searchBar.classList.remove("active");
+};
+
+document.querySelector("#cart-btn").onclick = () => {
+  cartList.classList.toggle("active");
+  navbar.classList.remove("active");
+  searchBar.classList.remove("active");
+};
+
+document.querySelector("#search-btn").onclick = () => {
+  searchBar.classList.toggle("active");
+  cartList.classList.remove("active");
+  navbar.classList.remove("active");
+};
+
+window.onscroll = () => {
+  navbar.classList.remove("active");
+  cartList.classList.remove("active");
+  searchBar.classList.remove("active");
+};
