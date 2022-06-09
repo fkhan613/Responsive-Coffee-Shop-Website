8c62538a96d2766c3ddb6737413ea248e48f8ebc
Farhan Khan <farhan.k2005@gmail.com>
2022-06-09 15:00:43 -0400

    made the cancel btn work in cart

--- index.js
+++ index.js
@@ -1,16 +1,76 @@
-let navbar = document.querySelector(".navbar");
-let cartList = document.querySelector(".cart-items-container");
-let searchBar = document.querySelector(".search-form");
-let item1 = document.querySelector("#item-1");
-let item2 = document.querySelector("#item-2");
-let item3 = document.querySelector("#item-3");
-let item4 = document.querySelector("#item-4");
-let item5 = document.querySelector("#item-5");
-let item6 = document.querySelector("#item-6");
-let item7 = document.querySelector("#item-7");
-let item8 = document.querySelector("#item-8");
-let item9 = document.querySelector("#item-9");
-let item10 = document.querySelector("#item-10");
+const navbar = document.querySelector(".navbar");
+const cartList = document.querySelector(".cart-items-container");
+const searchBar = document.querySelector(".search-form");
+const item1 = document.querySelector("#item-1");
+const item2 = document.querySelector("#item-2");
+const item3 = document.querySelector("#item-3");
+const item4 = document.querySelector("#item-4");
+const item5 = document.querySelector("#item-5");
+const item6 = document.querySelector("#item-6");
+const item7 = document.querySelector("#item-7");
+const item8 = document.querySelector("#item-8");
+const item9 = document.querySelector("#item-9");
+const item10 = document.querySelector("#item-10");
+const cancelBtn1 = document.querySelector(".item1cancel");
+const cancelBtn2 = document.querySelector(".item2cancel");
+const cancelBtn3 = document.querySelector(".item3cancel");
+const cancelBtn4 = document.querySelector(".item4cancel");
+const cancelBtn5 = document.querySelector(".item5cancel");
+const cancelBtn6 = document.querySelector(".item6cancel");
+const cancelBtn7 = document.querySelector(".item7cancel");
+const cancelBtn8 = document.querySelector(".item8cancel");
+const cancelBtn9 = document.querySelector(".item9cancel");
+const cancelBtn10 = document.querySelector(".item10cancel");
+
+cancelBtn1.onclick = () => {
+  item1.classList.remove("active");
+  document.querySelector(".item1btn").innerHTML = "Add to cart";
+};
+
+cancelBtn2.onclick = () => {
+  item2.classList.remove("active");
+  document.querySelector(".item2btn").innerHTML = "Add to cart";
+};
+
+cancelBtn3.onclick = () => {
+  item3.classList.remove("active");
+  document.querySelector(".item3btn").innerHTML = "Add to cart";
+}
+
+cancelBtn4.onclick = () => {
+  item4.classList.remove("active");
+  document.querySelector(".item4btn").innerHTML = "Add to cart";
+}
+
+cancelBtn5.onclick = () => {
+  item5.classList.remove("active");
+  document.querySelector(".item5btn").innerHTML = "Add to cart";
+}
+
+cancelBtn6.onclick = () => {
+  item6.classList.remove("active");
+  document.querySelector(".item6btn").innerHTML = "Add to cart";
+}
+
+cancelBtn7.onclick = () => {
+  item7.classList.remove("active");
+  document.querySelector(".item7btn").innerHTML = "Add to cart";
+}
+
+cancelBtn8.onclick = () => {
+  item8.classList.remove("active");
+  document.querySelector(".item8btn").innerHTML = "Add to cart";
+}
+
+cancelBtn9.onclick = () => {
+  item9.classList.remove("active");
+  document.querySelector(".item9btn").innerHTML = "Add to cart";
+}
+
+cancelBtn10.onclick = () => {
+  item10.classList.remove("active");
+  document.querySelector(".item10btn").innerHTML = "Add to cart";
+}
 
 document.querySelector("#item-1-btn").onclick = () => {
   item1.classList.toggle("active");
